fc9198423e2dac839fd4e3bf0af71cef39d23abb
mandevArr <86426254+mandevArr@users.noreply.github.com>
2021-07-21 11:03:15 -0500

    trying methods
    collaboration

--- src/main/java/org/Foo/Bar/EntitiesDao/UserDao.java
+++ src/main/java/org/Foo/Bar/EntitiesDao/UserDao.java
@@ -5,4 +5,8 @@ import org.Foo.Bar.Entities.User;
 public interface UserDao {
   void insertUser(User user);
   User findUserByEmail(String email);
+  User getUserById(Integer id);
+  void addUser(User user);
+  void deleteUser(Integer id);
+  List<User> getAllUsers();
 }
